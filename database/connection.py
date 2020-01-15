d0202623bebe57e14acedefdcfaee00c68c4dd1f
Nathan Cezar Cardoso <nathancezar@gmail.com>
2020-01-15 15:29:01 -0300

    fixed path to database

--- database/connection.py
+++ database/connection.py
@@ -4,7 +4,7 @@ from peewee import SqliteDatabase, Model
 dirpath = os.path.abspath(os.path.dirname(__file__))
 path = lambda string: os.path.join(dirpath, string)
 
-database = SqliteDatabase(path("/home/daniel/Documents/935/database/icm.db"), pragmas={
+database = SqliteDatabase(path("icm.db"), pragmas={
     'foreign_keys': 1,
 })
 
